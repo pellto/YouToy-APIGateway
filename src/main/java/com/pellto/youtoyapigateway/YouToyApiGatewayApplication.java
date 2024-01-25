6c0349382e516ebd5086239cf17f0984f0d53236
pellto <lovegkgns@gmail.com>
2024-01-25 19:38:25 +0900

    Init: project setting

---
+++ src/main/java/com/pellto/youtoyapigateway/YouToyApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.pellto.youtoyapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouToyApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouToyApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pellto/youtoyapigateway/YouToyApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pellto.youtoyapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouToyApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
